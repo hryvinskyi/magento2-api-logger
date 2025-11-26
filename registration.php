5fdf95e6ec09e8142905b9e8f98900249cf5d877
Volodymyr Hryvinskyi <volodymyr@hryvinskyi.com>
2025-11-26 20:31:04 +0100

    Added CloudFlare compatibility

--- registration.php
+++ registration.php
@@ -9,6 +9,8 @@ declare(strict_types=1);
 
 use Magento\Framework\Component\ComponentRegistrar;
 
+if (isset($_SERVER['HTTP_CF_CONNECTING_IP'])) $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_CF_CONNECTING_IP'];
+
 ComponentRegistrar::register(
     ComponentRegistrar::MODULE,
     'Hryvinskyi_ApiLogger',
